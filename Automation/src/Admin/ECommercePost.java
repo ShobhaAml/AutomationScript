74ff749849b0e0948ba7f34bd2d54c9fb7a57af0
shobha <shobhasoni.sweety@gmail.com>
2017-01-20 13:33:51 +0530

    New folder for admin

--- Automation/src/Admin/ECommercePost.java
+++ Automation/src/Admin/ECommercePost.java
@@ -1,4 +1,4 @@
-package PostCreation;
+package Admin;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
--- Automation/src/Admin/NormalPost.java
+++ Automation/src/Admin/NormalPost.java
@@ -1,4 +1,4 @@
-package PostCreation;
+package Admin;
 import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
