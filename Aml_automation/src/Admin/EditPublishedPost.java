61e17b0bd99c926f207b6b303e0ae5c118e3ac8d
shobha <shobhasoni.sweety@gmail.com>
2017-01-23 12:49:01 +0530

    Updated files

--- Aml_automation/src/Admin/EditPublishedPost.java
+++ Aml_automation/src/Admin/EditPublishedPost.java
@@ -1,5 +1,6 @@
 package Admin;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.Keys;
@@ -14,7 +15,8 @@ public class EditPublishedPost
     Adminproperty adminproperties = new Adminproperty();
     Properties prop = new Properties();
     WebDriver driver;
-    String Postname = "Automated eCommerce Post without homepage Image/content";
+    String Postname = "Slide show post testing:- Sumt";
+    String posttype = "Slide";
     Boolean status = true;
 
     public void Adminlogin() throws Exception
@@ -24,35 +26,72 @@ public class EditPublishedPost
                 prop.getProperty("browser"));
         adminproperties.adminLogin();
     }
-  @Test
+
+    @Test
     public void editPublishedPost() throws Exception
     {
         Adminlogin();
 
-        String catagory = adminproperties
-                .getcatagoryname("DashboardEditbuttontr", "DashboardCatagoryname", Postname);
-        
+        String catagory = adminproperties.getcatagoryname(
+                "DashboardEditbuttontr", "DashboardCatagoryname", Postname);
+
         System.out.println(catagory);
-        
+
         if (catagory.equalsIgnoreCase("Ecommerce")) {
             status = adminproperties.clickButton("DashboardEditbuttontr",
-                    "DashboardEditbuttontd", Postname,"Dashboardlist");
+                    "DashboardEditbuttontd", Postname, "Dashboardlist");
         } else {
             status = adminproperties.clickButton("DashboardEditbuttontr",
-                    "DashboardEditforotherpost", Postname,"Dashboardlist");
+                    "DashboardEditforotherpost", Postname, "Dashboardlist");
         }
 
         if (status == true) {
-            
-            String previous_text= adminproperties.findElement(prop.getProperty("post_content")).getText();
-            adminproperties.implicitWait();
-            adminproperties.findAndClick("post_content");
-            adminproperties.findElement(prop.getProperty("post_content")).clear();
-            adminproperties.findAndWrite("post_content",
-                    "Added new Data by Shobha  to test Edit feature"+previous_text);
-            adminproperties.findAndClick("post_title");
+
+            InsertPostdata();
+
+            if (posttype == "Slide") {
+                addSlides("\\src\\Images\\car.jpg");
+            }
+
         } else {
             Assert.assertFalse(false, "No Post with same Post title exists");
         }
     }
+
+    public void InsertPostdata()
+    {
+        String previous_text = adminproperties.findElement(
+                prop.getProperty("post_content")).getText();
+        adminproperties.implicitWait();
+        adminproperties.findAndClick("post_content");
+        adminproperties.findElement(prop.getProperty("post_content")).clear();
+        adminproperties.findAndWrite("post_content",
+                "Added new Data by Shobha  to test Edit feature"
+                        + previous_text);
+        adminproperties.findAndClick("post_title");
+    }
+
+    public void addSlides(String slide) throws Exception
+    {
+        adminproperties.findAndSendkey("post_content", Keys.END);
+        adminproperties.findAndSendkey("post_content", Keys.ENTER);
+        adminproperties.findAndClick("toolbar_slideshow");
+        adminproperties.implicitWait();
+        adminproperties.findAndClick("slideshow_addimage");
+        adminproperties.findAndClick("slideshow_input_image");
+        adminproperties.implicitWait();
+        Runtime.getRuntime().exec(
+                System.getProperty("user.dir")
+                        + "\\src\\DriverFiles\\fileupload.exe" + " "
+                        + System.getProperty("user.dir")
+                        + slide );
+        adminproperties.implicitWait();
+        adminproperties.findAndWrite("Slideshow_subtitle", "Slide 1");
+        adminproperties.findAndWrite("Slideshow_desc",
+                "Slide Description goes here");
+        adminproperties.findAndClick("slide_button");
+        adminproperties.findAndClick("slide_button_close");
+        adminproperties.implicitWait();
+    }
+
 }
