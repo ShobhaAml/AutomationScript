a3176507d318e24c174c1b03ff5f89b8588da4c1
SumitSharma011 <sumit@agilemedialab.in>
2017-04-03 11:41:32 +0530

    new folder

---
+++ Aml_automation/src/frontend/Login.java
@@ -0,0 +1,52 @@
+package Admin;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Verify;
+
+import Common.Adminproperty;
+import Common.Frontend;
+
+public class Login {
+	int invalidImageCount = 0;
+	WebDriver driver;
+	Frontend frontendProperties = new Frontend();
+	Properties prop = new Properties();
+	String Postdata, posttitle, postcontent, primaryimage, postcontent_more, images, productname, productOrder,
+			productDesc, productLink, productLinktext = "";
+	String productPrice, productCatagory, homecontent, homeImage, postcatagory, postcatagoryOther, tag, seotitle,
+			seodesc, specialpost = "";
+	String author, Twittertext, fbtext, allowHomepageImage, allowHomepageContent = "";
+	String browser = "";
+
+	@BeforeMethod
+	public void Setup() throws Exception {
+		prop = frontendProperties.ReadProperties();
+		driver = frontendProperties.frontcallproperty(prop.getProperty("url"), prop.getProperty("browser"));
+		browser = prop.getProperty("browser");
+	}
+
+	@DataProvider(name = "testdata")
+	public Object[][] TestDataFeed() throws Exception {
+		Object[][] postdata = frontendProperties.readExcel("Login", 2);
+		return postdata;
+	}
+
+	@Test(dataProvider = "testdata")
+	public void StandardLogin(String username, String password) {
+		String message = frontendProperties.StandardLogin(username, password);
+		System.out.println(message);
+	}
+
+}
