68eb2ccabdce0fc63ad1df948b0a47dc42ae8c30
ShobhaAml <shobhasoni.sweety@gmail.com>
2017-05-05 16:27:59 +0530

    Console log for individual link

--- Aml_automation/src/Frontend/consolemessage.java
+++ Aml_automation/src/Frontend/consolemessage.java
@@ -1,24 +1,10 @@
 package Frontend;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
-import java.util.logging.Level;
 
-import org.openqa.selenium.JavascriptExecutor;
+import java.util.Properties;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.logging.LogEntries;
-import org.openqa.selenium.logging.LogEntry;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import Common.Frontend;
 
 public class consolemessage
@@ -35,8 +21,7 @@ public class consolemessage
         driver = frontProperties.callproperty(prop.getProperty("url"),
                 prop.getProperty("browser"));
         browser = prop.getProperty("browser");
-        
-    }
+     }
     @Test
     public void testMethod() {
         driver.get(url);
