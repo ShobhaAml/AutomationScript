e852a5be9804b67dee5854d2e80fdd577c018a01
Ishagupta <isha@agilemedialab.in>
2017-07-07 11:28:10 +0530

    Updated files- DB_breakingTag, DB_postQueue

---
+++ Aml_automation/src/Frontend/DB_postQueue.java
@@ -0,0 +1,72 @@
+package Frontend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Common.Adminproperty;
+import Common.Frontend;
+
+public class DB_postQueue {
+	Frontend frontendProperties = new Frontend();
+	Adminproperty adminProperties = new Adminproperty();
+	Properties prop = new Properties();
+	String browser = "";
+	WebDriver driver;
+	String url = "https://guest:guest@testing.xataka.com";
+	String title;
+	List<String> list = new LinkedList<String>();
+	Connection conn;
+	Statement stmt;
+	List<String> DB_postList = new LinkedList<String>();
+	List<String> homeList = new LinkedList<String>();
+
+	@Test
+	public void openDB() throws Exception {
+		String query = "select p.post_title from wp_posts p, posts_queue q where p.id = q.post_id and q.rank < 17 and q.country = 'row' and p.post_status='publish' order by rank;";
+		conn = adminProperties.connectDb();
+		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		while (rs.next()) {
+			// String ID = rs.getString(1);
+			title = rs.getString(1);
+			DB_postList.add(title.trim());
+		}
+		System.out.println("Post list from database-" + DB_postList);
+		conn.close();
+	}
+
+	@Test
+	public void openbrowser() throws Exception {
+		prop = frontendProperties.ReadProperties();
+		driver = frontendProperties.frontcallproperty(url, prop.getProperty("browser"));
+		browser = prop.getProperty("browser");
+		frontendProperties.implicitWait();
+		java.util.List<WebElement> list = driver.findElements(By.xpath(prop.getProperty("postcommentlink")));
+		for (WebElement post : list) {
+			String postList = post.getText();
+			homeList.add(postList);
+		}
+		System.out.println("Post list on home page-" + homeList);
+		for (int i = 0; i < homeList.size(); i++) {
+			if (homeList.get(i).equals(DB_postList.get(i))) {
+				System.out.println("success");
+			} else
+				System.out.println("failure");
+		}
+
+	}
+}
