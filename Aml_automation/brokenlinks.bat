35e940250f65f2382fc79463f6cb31481a9359a6
Shobha <shobha@agilemedialab.in>
2019-03-29 18:40:56 +0530

    Fixes for bat file

--- Aml_automation/brokenlinks.bat
+++ Aml_automation/brokenlinks.bat
@@ -1,1 +1,1 @@
-java -cp E:\Project\Automation\*;E:\Project\Automation\Aml_automation\libs\* org.testng.TestNG E:\Project\Automation\Aml_automation\src\Brokenlink.xml
+java -cp E:\Project\Automation\Aml_automation\src ;E:\Project\Automation\Aml_automation\libs\* org.testng.TestNG E:\Project\Automation\Aml_automation\src\Brokenlink.xml
