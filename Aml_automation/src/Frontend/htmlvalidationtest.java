d9b12b2ad42505449d1453904e9dd538eb71a8b6
Shobha <shobha@agilemedialab.in>
2019-05-08 16:33:03 +0530

    HTML errors

--- Aml_automation/src/Frontend/htmlvalidationtest.java
+++ Aml_automation/src/Frontend/htmlvalidationtest.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
 
 import Common.Frontend;
 
-public class HTMLvalidation {
+public class htmlvalidationtest {
 	WebDriver driver;
 	Frontend frontendProperties = new Frontend();
 	Properties prop = new Properties();
