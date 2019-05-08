e5fbb26021b4da655fa7c84265116afabb4fa343
ShobhaAml <shobhasoni.sweety@gmail.com>
2019-05-08 15:25:37 +0530

    Update HTMLvalidations.bat

--- Aml_automation/HTMLvalidations.bat
+++ Aml_automation/HTMLvalidations.bat
@@ -1,1 +1,1 @@
-java -cp E:\Project\Automation\Aml_automation\bin;E:\Project\Automation\Aml_automation\bin\Frontend\src;E:\Project\Automation\Aml_automation\libs\* org.testng.TestNG src\HTMLvalidation.xml
+java -cp E:\Project\Automation\Aml_automation\bin;E:\Project\Automation\Aml_automation\libs\* org.testng.TestNG src\HTMLvalidation.xml
