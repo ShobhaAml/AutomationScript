f4c00aca6c8c156ad0d92821da20e1daa9c74311
Shobha <shobha@agilemedialab.in>
2019-05-13 10:47:24 +0530

    Batch file

--- Aml_automation/Ampvalidations.bat
+++ Aml_automation/Ampvalidations.bat
@@ -1,1 +1,1 @@
-java -cp E:\Project\Automation\Aml_automation\bin;E:\Project\Automation\Aml_automation\libs\* org.testng.TestNG src\Ampvalidation.xml
+java -cp C:\Users\shobha\.jenkins\workspace\AMPvalidations\Aml_automation\bin;C:\Users\shobha\.jenkins\workspace\AMPvalidations\Aml_automation\libs\* org.testng.TestNG src\Ampvalidation.xml
