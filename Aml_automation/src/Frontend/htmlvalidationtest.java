a17a2e07e3865dad5a8f5e3fb49eefc317253caa
ShobhaAml <shobhasoni.sweety@gmail.com>
2019-05-15 13:42:36 +0530

    Update htmlvalidationtest.java

--- Aml_automation/src/Frontend/htmlvalidationtest.java
+++ Aml_automation/src/Frontend/htmlvalidationtest.java
@@ -43,7 +43,7 @@ public class htmlvalidationtest {
 	        int cnt=1;
 	            for (WebElement element1 : list) {
 		           // System.out.println(element1.getAttribute("href"));
-		            if((cnt<10) && (!element1.getAttribute("href").contains("utm_campaign=repost"))) { 
+		            if((cnt<5) && (!element1.getAttribute("href").contains("utm_campaign=repost"))) { 
 		            	LocalerrorMap=getHTMLerror(element1.getAttribute("href"));
 		            	LocalerrorMap.forEach((k,v)->
             			{
