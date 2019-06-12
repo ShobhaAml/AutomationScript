d9469c3642b2e015bf94d637bb80bb99513fab3e
Shobha <shobha@agilemedialab.in>
2019-06-12 15:24:12 +0530

    Adding 1 file to compile

---
+++ Aml_automation/src/Frontend/headless.java
@@ -0,0 +1,28 @@
+package Frontend;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+public class headless {
+	
+	@Test
+	public void testbrowser()
+	{
+		
+	//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+	    chromeOptions.addArguments("--headless");
+	    chromeOptions.addArguments("--no-sandbox");
+	
+	    ChromeDriver driver = new ChromeDriver(chromeOptions);
+		driver.get("http://google.com");     
+	
+	     
+	    
+	}
+
+}
