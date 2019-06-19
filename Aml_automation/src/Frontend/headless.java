cc7e20a50ec3f0770a0ad15477c1d1711ad15c99
Isha13 <isha@agilemedialab.in>
2019-06-19 17:48:44 +0530

    updated headless class file and POM file

--- Aml_automation/src/Frontend/headless.java
+++ Aml_automation/src/Frontend/headless.java
@@ -14,13 +14,23 @@ public class headless {
 	public void testbrowser()
 	{
 		
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
+	/*  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("http://google.com");     
+		driver.get("http://google.com");  */
+		
+    	
+    		ChromeOptions options = new ChromeOptions();
+    		options.addArguments("windows-size=1400,800");
+    		options.addArguments("headless");
+            WebDriver driver = new ChromeDriver(options);
+            driver.get("http://www.google.com");
+            System.out.println(driver.getTitle());
+
+  
 	
 	     
 	    
