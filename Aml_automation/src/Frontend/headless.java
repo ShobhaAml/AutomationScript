e55825637896ab9a3380b6e1959ed317f1a98a00
Isha13 <isha@agilemedialab.in>
2019-06-19 18:21:49 +0530

    actual headless and pom files

--- Aml_automation/src/Frontend/headless.java
+++ Aml_automation/src/Frontend/headless.java
@@ -1,7 +1,5 @@
 package Frontend;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,26 +12,13 @@ public class headless {
 	public void testbrowser()
 	{
 		
-	/*  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("http://google.com");  */
-		
-    	
-    		ChromeOptions options = new ChromeOptions();
-    		options.addArguments("windows-size=1400,800");
-    		options.addArguments("headless");
-            WebDriver driver = new ChromeDriver(options);
-            driver.get("http://www.google.com");
-            System.out.println(driver.getTitle());
-
-  
-	
-	     
-	    
+		driver.get("http://google.com");     
 	}
 
 }
