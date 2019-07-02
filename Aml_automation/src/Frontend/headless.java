1049e3cd992cdd8d829491c0f744169c5894c913
Isha13 <isha@agilemedialab.in>
2019-07-02 13:26:59 +0530

    Minor change in headless file

--- Aml_automation/src/Frontend/headless.java
+++ Aml_automation/src/Frontend/headless.java
@@ -1,23 +1,31 @@
 package Frontend;
 
+
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
+
 public class headless {
 	
 	@Test
 	public void testbrowser()
-	{
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
-		ChromeOptions chromeOptions = new ChromeOptions();
-		//chromeOptions.setBinary("//src//Driverfiles//linux//chromedriver");
-		chromeOptions.addArguments("--no-sandbox");
-		chromeOptions.addArguments("--headless");
-		//chromeOptions.addArguments("--disable-dev-shm-usage");
-		//chromeOptions.setExperimentalOption("useAutomationExtension", false);
-		ChromeDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("http://google.com");
+	{	
+		
+		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//linux//chromedriver");
+		 ChromeOptions options = new ChromeOptions();
+    	 options.addArguments("windows-size=1400,800");
+      	 options.addArguments("headless");
+         WebDriver driver = new ChromeDriver(options);
+         driver.get("http://www.google.com");
+         System.out.println(driver.getTitle());
+
+  
+	
+	     
+	    
 	}
+
 }
