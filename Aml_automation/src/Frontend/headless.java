6a9ef176f5eba46199e6cda3a21c666a030c744d
Isha13 <isha@agilemedialab.in>
2019-07-09 18:44:12 +0530

    Added binary argument in headless file

--- Aml_automation/src/Frontend/headless.java
+++ Aml_automation/src/Frontend/headless.java
@@ -16,18 +16,22 @@ public class headless {
 	public void testbrowser()
 	{	
 		
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
 		ChromeOptions options = new ChromeOptions();
     	 //options.addArguments("windows-size=1400,800");
     	 //options.addArguments("–disable-dev-shm-usage");
+		options.addArguments("--start-maximized");
+		ChromeDriverManager.getInstance().setup();
+		options.addArguments("--dns-prefetch-disable");
+		options.addArguments("--always-authorize-plugins");
+
       	 options.addArguments("headless");
       	 options.addArguments("–disable-gpu");
-        WebDriver driver = new ChromeDriver(options);
-        ChromeDriverManager.getInstance().setup();
-         driver.get("http://www.google.com");
-         System.out.println("test");
+      	 options.setBinary(System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
+ 		//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
 
-	    
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://www.google.com");
+         System.out.println("test");    
 	}
 
 }
