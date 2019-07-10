077ca6e8faa8fd6c88d5d9ab5379134f2ada309b
Isha13 <isha@agilemedialab.in>
2019-07-10 13:08:29 +0530

    Removed chrome-driver manager dependency

--- Aml_automation/src/Frontend/headless.java
+++ Aml_automation/src/Frontend/headless.java
@@ -7,27 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-
-
 public class headless {
 	
 	@Test
 	public void testbrowser()
 	{	
-		
-		ChromeOptions options = new ChromeOptions();
-    	 //options.addArguments("windows-size=1400,800");
-    	 //options.addArguments("–disable-dev-shm-usage");
-		//options.addArguments("--start-maximized");
-		//ChromeDriverManager.getInstance().setup();
-		//options.addArguments("--dns-prefetch-disable");
-		//options.addArguments("--always-authorize-plugins");
-
-        options.addArguments("headless");
-      	// options.addArguments("–disable-gpu"); 
-      	 //options.setBinary(System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
  		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
+ 		ChromeOptions options = new ChromeOptions();
+      	// options.setBinary(System.getProperty("user.dir") + "//src//Driverfiles//chromedriver");
+ 		//options.addArguments("windows-size=1400,800");
+    	 //options.addArguments("–disable-dev-shm-usage");
+		options.addArguments("--start-maximized");
+		//ChromeDriverManager.getInstance().setup();
+		options.addArguments("--dns-prefetch-disable");
+		options.addArguments("--always-authorize-plugins");
+
+      	 options.addArguments("headless");
+      	 options.addArguments("–disable-gpu");
 
         WebDriver driver = new ChromeDriver(options);
         driver.get("http://www.google.com");
