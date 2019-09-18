20dfe4bfb0dfced82f68a279b1177e8549965d8f
dixitsoni2k19 <50621676+dixitsoni2k19@users.noreply.github.com>
2019-09-18 17:36:06 +0530

    Refactor lfeSanity

--- Aml_automation/src/AdminDashboardsanity/Longform_Dashboard_Sanity.java
+++ Aml_automation/src/AdminDashboardsanity/Longform_Dashboard_Sanity.java
@@ -1,9 +1,10 @@
-package Admin_Dashboard_sanity;
+package AdminDashboardsanity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,125 +17,210 @@ import Common.Adminproperty;
 public class Longform_Dashboard_Sanity {
 	WebDriver driver;
 	Adminproperty adminProperties = new Adminproperty();
-	CheckUserRoles roleType = new CheckUserRoles();
 	Properties prop = new Properties();
-	String blogname, browser, button, id, longformType, longformAutor;
-	int count=1;
-	
+	String blogname, browser, button, id, longformType, longformAutor, blogrole, role, author;
+	int count = 1;
+
 	@Test
-	public void Setup() throws Exception  {
+	public void Setup() throws Exception {
 		prop = adminProperties.ReadProperties();
 		driver = adminProperties.callproperty(prop.getProperty("url"), prop.getProperty("browser"));
 		browser = prop.getProperty("browser");
-		adminProperties.adminLogin();
-		roleType.openConnection(prop.getProperty("admin_usename"), prop.getProperty("admin_pwd"));
-		System.out.println("authorname and blogrole is:-----" + roleType.Authorname + "------"+roleType.blogrole);
-		longformDraft(roleType.blogrole);
-	}	
-	
-	    public void longformDraft(String role) throws Exception {
-	    	
+		adminProperties.LoginAdmin(prop.getProperty("dash_sanity_user"), prop.getProperty("dash_sanity_pass"));
+		blogrole = adminProperties.fetch_Role_Author();
+		String[] logintypes = blogrole.split("#");
+		author = logintypes[0].trim();
+		role = logintypes[1].trim();
+		longformDraft(role);
+	}
+
+	public void longformDraft(String role) throws Exception {
 		Actions action = new Actions(driver);
-		List<String> longformArray = new ArrayList<String>();	
-		List<String> longformPostTypeArray = new ArrayList<String>();	
-		List<String> longformAutorArray = new ArrayList<String>();	
+		List<String> longformArray = new ArrayList<String>();
+		List<String> longformPostTypeArray = new ArrayList<String>();
+		List<String> longformAutorArray = new ArrayList<String>();
 		List<WebElement> longformDraftlist = adminProperties.findElementsByXpath(prop.getProperty("longformDraftList"));
-		List<WebElement> longformDraftListBCollaborator = adminProperties.findElementsByXpath(prop.getProperty("longformDraftListBCollaborator"));
-    
-		if(role.equalsIgnoreCase("Branded Collaborator")){
-    	for(int j = 0; j<longformDraftListBCollaborator.size();j++){
-       	String[] getIDfromURL = adminProperties.findElement(prop.getProperty("longformDraftListBCollaborator") + "["+ (j + 1) + "]"+"/a").getAttribute("href").split("/"); 
-        longformArray.add(getIDfromURL[4]);}}
-        else{
-		for(int j = 0; j<longformDraftlist.size();j++)
-        {
-     	String[] getIDfromURL = adminProperties.findElement(prop.getProperty("longformDraftList") + "["+ (j + 1) + "]"+"/a").getAttribute("href").split("/");
-        longformArray.add(getIDfromURL[4]);}}  
-		
-        for(int l=0;l<longformArray.size();l++){
-        String[] splitText = "http://test.admin.weblogssl.com/edit/post/-L-1Tbjq0ClYVUddYrII?blog=xataka&userid=1".split("/");
-        String newString ="http://test.admin.weblogssl.com/edit/post/-L-1Tbjq0ClYVUddYrII?blog=xataka&userid=1".replaceAll(splitText[5].substring(0, 20), longformArray.get(l)); 
-        ((JavascriptExecutor)driver).executeScript("window.open();");
-        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-        driver.switchTo().window(tabs.get(1));
-        driver.get(newString);
-        Thread.sleep(1000);
-        
-        if(role.equalsIgnoreCase("Branded Collaborator")){
-           	 Thread.sleep(1000);
-        	 adminProperties.findElement(prop.getProperty("longformPublicar")).click();
-        	 Thread.sleep(2000);
-        	 if(adminProperties.findElement(prop.getProperty("longformBrandLabelBCollaborator")).getText().equalsIgnoreCase("Detalles de  Completa toda la  para que el equipo de WSL Branded Content pueda publicar el ")==true)
-             {longformType = "Brand";
-              longformPostTypeArray.add(longformType);}}
-        
-        else if(role.equalsIgnoreCase("Branded Coordinator")){
-        	adminProperties.findElement(prop.getProperty("longformPublicar")).click();
-        	Thread.sleep(2000);
-        	((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");        	
-        	adminProperties.findElement(prop.getProperty("longformBCoordinator"));           
-            Thread.sleep(2000);
-            longformType = "Brand";
-            longformPostTypeArray.add(longformType);            
-            longformAutor = adminProperties.findElement(prop.getProperty("longformAutor")).getAttribute("value");
-            longformAutorArray.add(longformAutor);}
-        
-        else if(role.equalsIgnoreCase("Unbranded Coordinator")||(role.equalsIgnoreCase("Editor"))||(role.equalsIgnoreCase("Unbranded collaboraor")))    
-    	    {Thread.sleep(2000);   
-	  	    if(driver.getCurrentUrl().contains("notAuthorized")){
-	        longformType = "Brand";           
-            longformPostTypeArray.add(longformType);
-	  	    }
-    	    else if(driver.getCurrentUrl().contains("notAuthorized")==false){
-    	    Thread.sleep(1000);
-    	    adminProperties.findElement(prop.getProperty("longformPublicar")).click();
-    		Thread.sleep(1000);
-    	    if(adminProperties.findElement(prop.getProperty("longformNormal")).getText().equalsIgnoreCase("Portada y redes sociales")==true);
-    	    {Thread.sleep(1000);
-            longformType = "Normal";           
-            longformPostTypeArray.add(longformType);  
-            ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
-    	    longformAutor = adminProperties.findElement(prop.getProperty("longformAutor")).getAttribute("value");
-            longformAutorArray.add(longformAutor); }}}
-            driver.close();
-            driver.switchTo().window(tabs.get(0));}
-             
-        for(int btn=0;btn<longformArray.size();btn++)
-        {
-        	if(role.equalsIgnoreCase("Branded Collaborator") && (longformType.equalsIgnoreCase("brand"))){ //B Collaborator
-            action.moveToElement(adminProperties.findElement(prop.getProperty("longformDraftListBCollaborator") + "[" + count + "]")).perform();
-            System.out.println("<<--PostTitle-->>    "+adminProperties.findElement(prop.getProperty("longformDraftListBCollaborator")+ "[" + count + "]" +"/a").getText()+"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<--Buttons-->>      "+adminProperties.findElement(prop.getProperty("longformDraftListBCollaborator")+"["+ count + "]"+ prop.getProperty("longformDraftListEditButton")).getText() +"     <<****PASS****>>");
-            }
-            
-        	if(role.equalsIgnoreCase("Branded Coordinator") && (longformType.equalsIgnoreCase("brand"))) { // B Coordinator
-        	((JavascriptExecutor) driver).executeScript("window.scrollBy(0,400)");
-           	 action.moveToElement(adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]")).perform();
-      	    System.out.println("<<--PostTitle-->>    "+adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()+"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<--Buttons-->>      "+adminProperties.findElement(prop.getProperty("longformDraftList")+"["+ count + "]"+ prop.getProperty("longformDraftListEditButton")).getText() +"     <<****PASS****>>");
-      	    }
-      	             
-            if(role.equalsIgnoreCase("Unbranded Coordinator")){  //UB Coordinator
-    	     if(longformPostTypeArray.get(btn).equalsIgnoreCase("normal")){
-          	((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
-          	action.moveToElement(adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]")).perform();
-         	System.out.println("<<--PostTitle-->>    " +adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()  +"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<--Buttons-->>      "+adminProperties.findElement(prop.getProperty("longformDraftList")+"["+ count + "]"+ prop.getProperty("longformDraftListEditButton")).getText()+"     <<****PASS****>>");}
-             else
-            System.out.println("<<--PostTitle-->>    " +adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()   +"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<----No action buttons on branded posts---->>          <<****PASS****>>");
-            }
-       
-            if(role.equalsIgnoreCase("Unbranded collaboraor")&& (longformPostTypeArray.get(btn).equalsIgnoreCase("normal"))){ //UB Collaborator
-             if(longformAutorArray.get(btn).equalsIgnoreCase(roleType.Authorname)){
-    	    ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,400)");
-    	    action.moveToElement(adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]")).perform();
-        	System.out.println("<<--PostTitle-->>    " +adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()  +"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<--Buttons-->>      "+adminProperties.findElement(prop.getProperty("longformDraftList")+"["+ count + "]"+ prop.getProperty("longformDraftListEditButton")).getText()+"     <<****PASS****>>");}
-         	 else
-            System.out.println("<<--PostTitle-->>    " +adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()   +"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<----No action buttons on other's post---->>         ");
-            }
-        		
-        	if(role.equalsIgnoreCase("Editor")&&(longformPostTypeArray.get(btn).equalsIgnoreCase("normal"))){ //Editor
-            ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
-    	    action.moveToElement(adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]")).perform();
-          	System.out.println("<<--PostTitle-->>    " +adminProperties.findElement(prop.getProperty("longformDraftList")+ "[" + count + "]" +"/a").getText()  +"        <<--PostType-->>"     +longformPostTypeArray.get(btn) +"        <<--Buttons-->>      "+adminProperties.findElement(prop.getProperty("longformDraftList")+"["+ count + "]"+ prop.getProperty("longformDraftListEditButton")).getText()+"     <<****PASS****>>");
-          	}
-            count++; }       
-        }
+
+		if (role.equalsIgnoreCase("Bcol")) {
+			List<WebElement> longformDraftListBCollaborator = adminProperties
+					.findElementsByXpath(prop.getProperty("longformDraftListBCollaborator"));
+			for (int j = 0; j < longformDraftListBCollaborator.size(); j++) {
+				String[] getIDfromURL = adminProperties
+						.findElement(prop.getProperty("longformDraftListBCollaborator") + "[" + (j + 1) + "]" + "/a")
+						.getAttribute("href").split("/");
+				longformArray.add(getIDfromURL[4]);
+			}
+		} else {
+			for (int j = 0; j < longformDraftlist.size(); j++) {
+
+				String[] getIDfromURL = adminProperties
+						.findElement(prop.getProperty("longformDraftList") + "[" + (j + 1) + "]" + "/a")
+						.getAttribute("href").split("/");
+				longformArray.add(getIDfromURL[4]);
+			}
+		}
+
+		for (int l = 0; l < longformArray.size(); l++) {
+
+			String firebaseID = longformArray.get(l);
+			String newurl = "http://test.admin.weblogssl.com/edit/post/" + firebaseID + "?blog=xataka&userid=1";
+			((JavascriptExecutor) driver).executeScript("window.open();");
+			ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+			driver.switchTo().window(tabs.get(1));
+			driver.get(newurl);
+			Thread.sleep(1000);
+
+			if (role.equalsIgnoreCase("Bcol")) {
+				Thread.sleep(1000);
+				adminProperties.findElement(prop.getProperty("longformPublicar")).click();
+				Thread.sleep(2000);
+				if (adminProperties.findElement(prop.getProperty("longformBrandLabelBCollaborator")).getText()
+						.equalsIgnoreCase(
+								"Detalles de  Completa toda la  para que el equipo de WSL Branded Content pueda publicar el ") == true) {
+					longformType = "Brand";
+					longformPostTypeArray.add(longformType);
+				}
+			}
+
+			else if (role.equalsIgnoreCase("BC")) {
+				adminProperties.findElement(prop.getProperty("longformPublicar")).click();
+				Thread.sleep(2000);
+				// ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
+				adminProperties.findElement(prop.getProperty("longformBCoordinator"));
+				Thread.sleep(2000);
+				longformType = "Brand";
+				longformPostTypeArray.add(longformType);
+				longformAutor = adminProperties.findElement(prop.getProperty("longformAutor")).getAttribute("value");
+				longformAutorArray.add(longformAutor);
+			}
+
+			else if (role.equalsIgnoreCase("UBC") || (role.equalsIgnoreCase("Editor"))
+					|| (role.equalsIgnoreCase("UbCol"))) {
+				Thread.sleep(2000);
+				if (driver.getCurrentUrl().contains("notAuthorized")) {
+					longformType = "Brand";
+					longformPostTypeArray.add(longformType);
+				} else if (driver.getCurrentUrl().contains("notAuthorized") == false) {
+					Thread.sleep(1000);
+					adminProperties.findElement(prop.getProperty("longformPublicar")).click();
+					Thread.sleep(1000);
+					if (adminProperties.findElement(prop.getProperty("longformNormal")).getText()
+							.equalsIgnoreCase("Portada y redes sociales") == true)
+						;
+					{
+						Thread.sleep(1000);
+						longformType = "Normal";
+						longformPostTypeArray.add(longformType);
+						// ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
+						longformAutor = adminProperties.findElement(prop.getProperty("longformAutor"))
+								.getAttribute("value");
+						longformAutorArray.add(longformAutor);
+					}
+				}
+			}
+			driver.close();
+			driver.switchTo().window(tabs.get(0));
+		}
+
+		for (int btn = 0; btn < longformArray.size(); btn++) {
+			if (role.equalsIgnoreCase("Bcol") && (longformType.equalsIgnoreCase("brand"))) { // B Collaborator
+				action.moveToElement(adminProperties
+						.findElement(prop.getProperty("longformDraftListBCollaborator") + "[" + count + "]")).perform();
+				System.out.println("<<--PostTitle-->>    "
+						+ adminProperties
+								.findElement(
+										prop.getProperty("longformDraftListBCollaborator") + "[" + count + "]" + "/a")
+								.getText()
+						+ "        <<--PostType-->>" + longformPostTypeArray.get(btn) + "        <<--Buttons-->>      "
+						+ adminProperties.findElement(prop.getProperty("longformDraftListBCollaborator") + "[" + count
+								+ "]" + prop.getProperty("longformDraftListEditButton")).getText()
+						+ "     <<****PASS****>>");
+			}
+
+			if (role.equalsIgnoreCase("BC") && (longformType.equalsIgnoreCase("brand"))) { // B Coordinator
+				((JavascriptExecutor) driver).executeScript("window.scrollBy(0,400)");
+				action.moveToElement(
+						adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"))
+						.perform();
+				System.out
+						.println("<<--PostTitle-->>    " + adminProperties
+								.findElement(prop.getProperty("longformDraftList") + "[" + count + "]" + "/a").getText()
+								+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+								+ "        <<--Buttons-->>      "
+								+ adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"
+										+ prop.getProperty("longformDraftListEditButton")).getText()
+								+ "     <<****PASS****>>");
+			}
+
+			if (role.equalsIgnoreCase("UBC")) { // UB Coordinator
+				if (longformPostTypeArray.get(btn).equalsIgnoreCase("normal")) {
+					// ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
+					action.moveToElement(
+							adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"))
+							.perform();
+					System.out.println(
+							"<<--PostTitle-->>    "
+									+ adminProperties
+											.findElement(
+													prop.getProperty("longformDraftList") + "[" + count + "]" + "/a")
+											.getText()
+									+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+									+ "        <<--Buttons-->>      "
+									+ adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count
+											+ "]" + prop.getProperty("longformDraftListEditButton")).getText()
+									+ "     <<****PASS****>>");
+				} else
+					System.out.println("<<--PostTitle-->>    " + adminProperties
+							.findElement(prop.getProperty("longformDraftList") + "[" + count + "]" + "/a").getText()
+							+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+							+ "        <<----No action buttons on branded posts---->>          <<****PASS****>>");
+			}
+
+			if (role.equalsIgnoreCase("UbCol") && (longformPostTypeArray.get(btn).equalsIgnoreCase("normal"))) {
+				// UBCollaborator
+				if (longformAutorArray.get(btn).equalsIgnoreCase(author)) {
+					((JavascriptExecutor) driver).executeScript("window.scrollBy(0,400)");
+					action.moveToElement(
+							adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"))
+							.perform();
+					System.out.println(
+							"<<--PostTitle-->>    "
+									+ adminProperties
+											.findElement(
+													prop.getProperty("longformDraftList") + "[" + count + "]" + "/a")
+											.getText()
+									+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+									+ "        <<--Buttons-->>      "
+									+ adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count
+											+ "]" + prop.getProperty("longformDraftListEditButton")).getText()
+									+ "     <<****PASS****>>");
+				} else
+					System.out
+							.println("<<--PostTitle-->>    "
+									+ adminProperties
+											.findElement(
+													prop.getProperty("longformDraftList") + "[" + count + "]" + "/a")
+											.getText()
+									+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+									+ "        <<----No action buttons on other's post---->>         ");
+			}
+
+			if (role.equalsIgnoreCase("Editor") && (longformPostTypeArray.get(btn).equalsIgnoreCase("normal"))) { // Editor
+				// ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,600)");
+
+				action.moveToElement(
+						adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"))
+						.perform();
+				System.out
+						.println("<<--PostTitle-->>    " + adminProperties
+								.findElement(prop.getProperty("longformDraftList") + "[" + count + "]" + "/a").getText()
+								+ "        <<--PostType-->>" + longformPostTypeArray.get(btn)
+								+ "        <<--Buttons-->>      "
+								+ adminProperties.findElement(prop.getProperty("longformDraftList") + "[" + count + "]"
+										+ prop.getProperty("longformDraftListEditButton")).getText()
+								+ "     <<****PASS****>>");
+			}
+			count++;
+		}
+	}
 }
