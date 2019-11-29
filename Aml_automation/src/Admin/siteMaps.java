7181c830b817962f60adda8f1e3e4632c75fa85d
Isha13 <isha@agilemedialab.in>
2019-11-29 14:51:50 +0530

    Script to fetch sitemap URL's, dependency into POM file

---
+++ Aml_automation/src/Admin/siteMaps.java
@@ -0,0 +1,44 @@
+package Admin;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import javax.xml.parsers.ParserConfigurationException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+import org.jsoup.select.Elements;
+import org.xml.sax.SAXException;
+
+public class siteMaps {
+
+	WebDriver driver;
+	List<String> list = new ArrayList<String>();
+	String siteMap_URL = "https://guest:guest@testing.xataka.com/sitemap_index.xml";
+
+	@Test
+	public void fetch_siteMap_URL() throws InterruptedException, ParserConfigurationException, SAXException, IOException {
+		ChromeOptions opt = new ChromeOptions();
+		System.setProperty("webdriver.chrome.driver", "//usr//local//bin//chromedriver 1.48.21 PM");
+		driver = new ChromeDriver(opt);
+		opt.addArguments("start-maximized");
+		driver.get(siteMap_URL);
+		driver.manage().timeouts().implicitlyWait(200, TimeUnit.SECONDS);
+
+		String src = driver.getPageSource();
+
+		Elements parse_element = org.jsoup.Jsoup.parse(src).getElementsByClass("text");
+		for (org.jsoup.nodes.Element item : parse_element) {
+			if (item.text().contains("https"))
+				list.add(item.unwrap().toString());
+		}
+		driver.close();
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+}
